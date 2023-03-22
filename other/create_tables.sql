d10aefc62ee4f1f8dd7df1951566b064af728a61
Yan <100032599+whykay-01@users.noreply.github.com>
2023-03-22 23:11:05 +0800

    fix: wip make connection to the database work properly

--- other/create_tables.sql
+++ other/create_tables.sql
@@ -1,7 +1,9 @@
--- first of all create a DB
-create database reservation_system;
 -- in case of failure run the query to drop the DB and run the queries again
 -- drop database reservation_system;
+
+-- use reservation_system;
+
+-- create database reservation_system;
 
 create table airport(
     name varchar(255) primary key not null,
