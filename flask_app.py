e5615b836c530f99c637dd467ae4c3f0f1b25ac2
Yan <100032599+whykay-01@users.noreply.github.com>
2023-05-28 20:13:21 +0700

    feat: rearranged the order for draw_barchart func

--- flask_app.py
+++ flask_app.py
@@ -57,6 +57,35 @@ print("Connected to database:", data[0])
 cursor.close()
 
 
+def draw_barchart(sql_data, title, xlabel, ylabel, filename, color, name):
+    total_amount = []
+    labels = []
+    for row in sql_data:
+        labels.append(row[0])
+        total_amount.append(row[1])
+
+    # Create a vertical bar chart
+    with plt.ion():
+        fig, ax = plt.subplots(figsize=(10, 6))
+        ax.bar(labels, total_amount, color=color)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.set_title(title)
+        ax.set_xticklabels(labels)
+
+        # Display the exact value of each bar
+        for i, v in enumerate(total_amount):
+            ax.text(i, v / 2, "{:.0f}".format(v), ha="center", fontsize=12)
+
+    filename = name + ".png"
+
+    file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+    fig.savefig(file_path)
+    plt.close(fig)
+
+    return filename
+
+
 @app.route("/")
 def welcome():
     if "username" in session:
